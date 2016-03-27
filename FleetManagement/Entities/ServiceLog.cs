1ea204b79de96e8eab74ee398a7ab514b7c56950
mitesh-diligencevault <mitesh@diligencevault.com>
2016-03-28 03:27:11 +0530

    servicelog enity & service class.

---
+++ FleetManagement/Entities/ServiceLog.cs
@@ -0,0 +1,77 @@
+﻿using FleetManagement.Common;
+using FleetManagement.Enums;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace FleetManagement.Entities
+{
+    public class ServiceLog : BaseEntity
+    {
+        public int Id { get; set; }
+
+        public int VehicleID { get; set; }
+
+        public DateTime SentDate { get; set; }
+
+        public int MileageReading { get; set; }
+
+        public string ServiceReason { get; set; }
+
+        public decimal? SpareExpenditure { get; set; }
+
+        public decimal? LabourExpenditure { get; set; }
+
+        public DateTime NextServiceDate { get; set; }
+
+        public DateTime InsertTimeStamp { get; set; }
+
+        public bool Manage()
+        {
+            int success = 0;
+            if (ObjState == ObjectState.DELETE)
+            {
+                success = SqlHelper.ExecuteNonQuery("Delete_ServiceLog", getSqlParameters().ToArray());
+            }
+            else
+            {
+                success = SqlHelper.ExecuteNonQuery("Manage_ServiceLog", getSqlParameters().ToArray());
+            }
+            return success > -1;
+        }
+        private List<SqlParameter> getSqlParameters()
+        {
+            List<SqlParameter> sqlParamList = new List<SqlParameter>();
+
+            if (ObjState == ObjectState.DELETE)
+            {
+                sqlParamList.Add(new SqlParameter("@Id", Id));
+            }
+            else
+            {
+                sqlParamList.Add(new SqlParameter("@VehicleID", VehicleID));
+                sqlParamList.Add(new SqlParameter("@SentDate", SentDate));
+                sqlParamList.Add(new SqlParameter("@MileageReading", MileageReading));
+                sqlParamList.Add(new SqlParameter("@ServiceReason", ServiceReason));
+                sqlParamList.Add(new SqlParameter("@SpareExpenditure", SpareExpenditure));
+                sqlParamList.Add(new SqlParameter("@LabourExpenditure", LabourExpenditure));
+                sqlParamList.Add(new SqlParameter("@NextServiceDate", NextServiceDate));
+                sqlParamList.Add(new SqlParameter("@ObjectState", this.ObjState));
+
+            }
+            return sqlParamList;
+
+        }
+        public bool Validate()
+        {
+            if (this.Id <= 0)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+
+}
---
+++ FleetManagement/Services/ServiceLogService.cs
@@ -0,0 +1,75 @@
+﻿using FleetManagement.Entities;
+using FleetManagement.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FleetManagement.Common;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace FleetManagement.Services
+{
+    public class ServiceLogService : IEntityService<ServiceLog>
+    {
+        public List<ServiceLog> Get(int ID = 0)
+        {
+            List<SqlParameter> sqlParams = new List<SqlParameter>();
+            List<ServiceLog> serviceLogs = new List<ServiceLog>();
+            if (ID > 0)
+            {
+                sqlParams.Add(new SqlParameter("CustomerID", ID));
+            }
+            DataSet ds = SqlHelper.ExecuteDataset("Get_Customer", sqlParams.ToArray());
+            if (ds.ValidDataSet())
+            {
+                foreach (DataRow row in ds.Tables[0].Rows)
+                {
+                    serviceLogs.Add(Fill(row));
+                }
+            }
+            return serviceLogs;
+        }
+
+        public bool Insert(ServiceLog obj)
+        {
+            obj.ObjState = Enums.ObjectState.INSERT;
+            return obj.Manage();
+        }
+
+        public bool Update(ServiceLog obj)
+        {
+            obj.ObjState = Enums.ObjectState.UPDATE;
+            return obj.Manage();
+        }
+
+        public bool Delete(int ID)
+        {
+            ServiceLog obj = new ServiceLog();
+            obj.Id = ID;
+            obj.ObjState = Enums.ObjectState.DELETE;
+            if (obj.Validate())
+            {
+                return obj.Manage();
+            }
+            return false;
+        }
+
+        public ServiceLog Fill(System.Data.DataRow item)
+        {
+            if (item == null) return null;
+            ServiceLog serviceLog = new ServiceLog();
+            serviceLog.Id                   = item.GetIntValue("ID");
+            serviceLog.VehicleID            = item.GetIntValue("VehicleID");
+            serviceLog.SentDate             = item.GetDateTimeValue("SentDate");
+            serviceLog.MileageReading       = item.GetIntValue("MileageReading");
+            serviceLog.ServiceReason        = item.GetValue("ServiceReason");
+            serviceLog.SpareExpenditure     = item.GetDecimalValue("SpareExpenditure");
+            serviceLog.LabourExpenditure    = item.GetDecimalValue("LabourExpenditure");
+            serviceLog.NextServiceDate      = item.GetDateTimeValue("NextServiceDate");
+            serviceLog.InsertTimeStamp      = item.GetDateTimeValue("InsertTimeStamp");
+            return serviceLog;
+        }
+    }
+}
